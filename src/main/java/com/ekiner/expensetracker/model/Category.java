bcaaec0100e39f376c25e4b06c537128c436bb68
Efe <mehmetefeekiner@gmail.com>
2020-06-29 01:27:57 +0200

    Create controller

--- src/main/java/com/ekiner/expensetracker/model/Category.java
+++ src/main/java/com/ekiner/expensetracker/model/Category.java
@@ -9,6 +9,7 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +21,7 @@ public class Category {
 	@Id
 	private Long id;
 	
+	@NonNull
 	private String name;
 	
 }
